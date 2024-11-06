55f2bb617ab452fc4a68a5077fe5ea4c358b64bf
David <davidpolo235@gmail.com>
2024-11-06 14:54:32 -0300

    anadi superavit

--- src/main/java/aed/BestEffort.java
+++ src/main/java/aed/BestEffort.java
@@ -9,9 +9,13 @@ public class BestEffort {
     private Traslado[] traslados2;
     private Heap heapGananciaNeta;
     private Heap heapTimestamp;
+    private int[] mayorSuperavit;
+    private ArrayList mayorGanacia;
+    private ArrayList mayorPerdida;
 
     public BestEffort(int cantCiudades, Traslado[] traslados){
         array = new Ciudad[cantCiudades];
+        mayorSuperavit =  new int[2];
         int i = 0;
         while (i < cantCiudades) {  // O(|cantCiudades|)
             array[i] = new Ciudad(0, 0, 0);  // Crear y asignar nuevas ciudades
@@ -37,15 +41,61 @@ public class BestEffort {
 
     public int[] despacharMasRedituables(int n){
         int[] res = new int[n];
+        
+        int[] anadir = new int[2];
         int i = 0;
         while (i<n) {
             Traslado maxGanancia = heapGananciaNeta.sacarRaiz();
             res[i] = maxGanancia.id;
 
-            heapTimestamp.eliminarElemento(maxGanancia);
+            array[maxGanancia.origen].ganancia += maxGanancia.gananciaNeta;
+            array[maxGanancia.destino].perdida += maxGanancia.gananciaNeta;
+
+            array[maxGanancia.origen].superavit = array[maxGanancia.origen].ganancia - array[maxGanancia.origen].perdida;
+            array[maxGanancia.destino].superavit = array[maxGanancia.destino].ganancia -array[maxGanancia.destino].perdida;
+
+            // sacar el mayorsuperavir de las dos ciudades despachadas
+            if (array[maxGanancia.origen].superavit > array[maxGanancia.destino].superavit){
+                anadir[0] = maxGanancia.origen;
+                anadir[1] = array[maxGanancia.origen].superavit;
+            }
+            else if (array[maxGanancia.origen].superavit < array[maxGanancia.destino].superavit) {
+                anadir[0] = maxGanancia.destino;
+                anadir[1] = array[maxGanancia.destino].superavit;
+            }
+            else if (maxGanancia.origen > maxGanancia.destino) {
+                anadir[0] = maxGanancia.destino;
+                anadir[1] = array[maxGanancia.destino].superavit;
+            }
+            else{
+                anadir[0] = maxGanancia.origen;
+                anadir[1] = array[maxGanancia.origen].superavit;
+            }
+
+            // saca el mayor o mayores --> mayoresGanacias de los dos despachos
+
+
+
+
+            if (mayorSuperavit.length == 0) {
+                mayorSuperavit[0] = anadir[0];
+                mayorSuperavit[1] = anadir [1];
+            }
+            else if(mayorSuperavit[1] < anadir[1]){
+                mayorSuperavit[0] = anadir[0]; 
+                mayorSuperavit[1] = anadir[1];
+            }
+            else if(mayorSuperavit[1] == anadir[1]){
+                if(anadir[0] < mayorSuperavit[0]){
+                    mayorSuperavit[0] = anadir[0]; 
+                    mayorSuperavit[1] = anadir[1];
+                }
+            }
+
+            heapTimestamp.eliminarElemento(maxGanancia.puntero);
             i++;
         }
-        
+
         return res;
     }
 
